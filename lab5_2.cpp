18b7698de79320770c2d5bc6eb2974bb7d4fdf9b
lab302-23 <lab302-23@labeng.local>
2020-01-07 16:23:42 +0700

    Done

--- lab5_2.cpp
+++ lab5_2.cpp
@@ -1,6 +1,24 @@
 #include<iostream>
+#include<string>
 using namespace std;
 
+string mixText(string s1,string s2){
+	int L1 = s1.size();
+	int L2= s2.size();
+	int L = L1+L2;
+	string s ="";
+	if(L1 != L2){
+		cout << "E";
+
+	}else{
+		int i = 0;
+		while(i < L){
+			s = s + s1[i] + s2[i];
+			i++;
+		}
+		return s;
+	}
+}
 
 
 
